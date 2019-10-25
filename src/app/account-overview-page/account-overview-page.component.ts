d67acadc2643d14b2bf4c28d8f5a2071cd3dfac4
Baraghen <samir.blom@gmail.com>
2019-10-25 17:29:56 +0200

    added values to account page

--- src/app/account-overview-page/account-overview-page.component.ts
+++ src/app/account-overview-page/account-overview-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountDataService } from '../services/account-data.service';
 import { ActivatedRoute } from '@angular/router';
-import { Account } from '../classes/account';
+import { FilterValues } from '../classes/filterValues';
+
 
 @Component({
   selector: 'app-account-overview-page',
@@ -12,9 +13,19 @@ export class AccountOverviewPageComponent implements OnInit {
 
   constructor(private accountDataService: AccountDataService, private route: ActivatedRoute) { }
 
-  accountData = {} as Account;
+  accountData;
 
   showContent:boolean = false;
+
+  convertToDate(dateString) {
+    if(dateString !== null) {
+      let d = new Date(Number(dateString)).toLocaleString('en-SE', this.options)
+      return d
+    }
+    else {
+      return 'No date available';
+    }
+  }
 
   options = {
     weekday: 'long', 
@@ -26,12 +37,12 @@ export class AccountOverviewPageComponent implements OnInit {
 
   d = new Date(1562324951475).toLocaleString('en-SE', this.options);
 
-  getData(params?:object) {
-    this.accountDataService.filterAccounts(params).subscribe(
+  getData(params?:FilterValues) {
+    this.accountDataService.getAllAccountData(params).subscribe(
       data => {
-        this.accountData = data[0];
-        console.log(this.accountData)
+        this.accountData = data;
         this.showContent = true;
+        console.log(this.accountData)
       }
     )
   }
@@ -40,8 +51,14 @@ export class AccountOverviewPageComponent implements OnInit {
 
     let id = parseInt(this.route.snapshot.paramMap.get('id'));
 
-    let filterValues:Object = {
-      id: id
+    let filterValues:FilterValues = {
+      account: {
+        id: id
+      },
+      info: {
+        site_id: id
+      }
+      
     }
 
     this.getData(filterValues)
